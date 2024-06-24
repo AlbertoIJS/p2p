8129f94dd87c8016d67815a17986753b4d2af545
AlbertoIJS <aijs.cdmx@gmail.com>
2024-06-23 21:16:59 -0600

    Implementar funcion de descarga

--- main.go
+++ main.go
@@ -1,3 +1,60 @@
-package p2p
+package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"sync"
+)
+
+type File struct {
+	Name string
+	Data []byte
+}
+
+type Node struct {
+	Files []File
+}
+
+var nodes []Node
+var mutex = &sync.Mutex{}
+
+// Búsqueda por índices locales
+func searchFile(fileName string) *File {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	for _, node := range nodes {
+		for _, file := range node.Files {
+			if file.Name == fileName {
+				return &file
+			}
+		}
+	}
+
+	return nil
+}
+
+func downloadFile(file *File) {
+	err := os.WriteFile(file.Name, file.Data, 0644)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+		return
+	}
+	fmt.Println("File downloaded:", file.Name)
+}
+
+func main() {
+	// Añade nodos y archivos a la red
+	nodes = append(nodes, Node{Files: []File{{Name: "file1", Data: []byte("data1")}}})
+	nodes = append(nodes, Node{Files: []File{{Name: "file2", Data: []byte("data2")}}})
+
+	// Buscar un archivo en la red
+	fileName := "file0"
+	file := searchFile(fileName)
+	if file != nil {
+		fmt.Printf("Archivo encontrado: %s\n", file.Name)
+		//downloadFile(file)
+	} else {
+		fmt.Println("Archivo no encontrado")
+	}
+}
